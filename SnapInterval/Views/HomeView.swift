8f4d08eb8af5296cfa6fc371950a1f5e9ef06c43
Stanley Pan <imoment.developer@gmail.com>
2022-01-28 15:31:56 -0500

    Extract view to ScrollContent, layout adjustments

--- SnapInterval/Views/HomeView.swift
+++ SnapInterval/Views/HomeView.swift
@@ -12,22 +12,42 @@ struct HomeView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             
-            VStack(spacing: 0) {
-                ForEach(links) { link in
-                    
-                    HStack {
+            ScrollContent()
+                .padding(.top, getScreenSize().height / 3)
+                .padding(.bottom, CGFloat(links.count - 1) * 80)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        // MARK: Overlay Masking View
+        .background(
+            Color("yellow")
+                .frame(height: 80)
+                .padding(.top, getScreenSize().height / 3)
+            ,alignment: .top
+        )
+        .background(Color("background"))
+    }
+    
+    @ViewBuilder
+    func ScrollContent(showTitle: Bool = false) -> some View {
+        VStack(spacing: 0) {
+            ForEach(links) { link in
+                HStack {
+                    if showTitle {
+                        Text(link.title)
+                            .font(.title2.bold())
+                            .foregroundColor(Color("background"))
+                    } else {
                         Spacer()
-                        
-                        Image(systemName: link.logo)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 40, height: 40)
-                            .foregroundColor(Color("yellow"))
-                            
                     }
-                    .padding(.horizontal, 15)
-                    .frame(height: 80)
+                    
+                    Image(systemName: link.logo)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(showTitle ? Color("background") : Color("yellow"))
                 }
+                .padding(.horizontal, 15)
+                .frame(height: 80)
             }
         }
     }
@@ -36,5 +56,11 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+    }
+}
+
+extension View {
+    func getScreenSize() -> CGRect {
+        return UIScreen.main.bounds
     }
 }
