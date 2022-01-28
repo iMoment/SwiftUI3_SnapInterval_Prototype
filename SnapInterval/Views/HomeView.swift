602b1caecf375a33dd9e36b6c45183d46696eb32
Stanley Pan <imoment.developer@gmail.com>
2022-01-28 15:40:03 -0500

    Added OffsetModifier and OffsetKey

--- SnapInterval/Views/HomeView.swift
+++ SnapInterval/Views/HomeView.swift
@@ -15,27 +15,32 @@ struct HomeView: View {
             ScrollContent()
                 .padding(.top, getScreenSize().height / 3)
                 .padding(.bottom, CGFloat(links.count - 1) * 80)
+                // MARK: Overlay Masking View
+                .overlay(
+                    GeometryReader { _ in
+                        ScrollContent(showTitle: true)
+                    }
+                    .frame(height: 80)
+                    .clipped()
+                    .background(Color("yellow"))
+                    .padding(.top, getScreenSize().height / 3)
+                    ,alignment: .top
+                )
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-        // MARK: Overlay Masking View
-        .background(
-            Color("yellow")
-                .frame(height: 80)
-                .padding(.top, getScreenSize().height / 3)
-            ,alignment: .top
-        )
         .background(Color("background"))
     }
     
     @ViewBuilder
     func ScrollContent(showTitle: Bool = false) -> some View {
-        VStack(spacing: 0) {
+        VStack(spacing:  0) {
             ForEach(links) { link in
                 HStack {
                     if showTitle {
                         Text(link.title)
                             .font(.title2.bold())
                             .foregroundColor(Color("background"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
                     } else {
                         Spacer()
                     }
@@ -62,5 +67,32 @@ struct HomeView_Previews: PreviewProvider {
 extension View {
     func getScreenSize() -> CGRect {
         return UIScreen.main.bounds
+    }
+}
+
+// MARK: ScrollView Content Offset
+struct OffsetModifier: ViewModifier {
+    @Binding var offset: CGFloat
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+                GeometryReader { proxy in
+                    Color.clear
+                        .preference(key: OffsetKey.self, value: proxy.frame(in: .named("SCROLL")).minY)
+                }
+            }
+            .onPreferenceChange(OffsetKey.self) { value in
+                self.offset = value
+            }
+    }
+}
+
+// MARK: Content Offset Preference Key
+struct OffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+    
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
     }
 }
