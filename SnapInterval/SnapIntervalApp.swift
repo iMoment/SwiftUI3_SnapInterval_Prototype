b62d3b53549534edad7d8491194dd24bf286883b
Stanley Pan <imoment.developer@gmail.com>
2022-01-28 14:40:18 -0500

    Initial Commit

---
+++ SnapInterval/SnapIntervalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapIntervalApp.swift
+//  SnapInterval
+//
+//  Created by Stanley Pan on 2022/01/28.
+//
+
+import SwiftUI
+
+@main
+struct SnapIntervalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
